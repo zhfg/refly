30f07a0c10f548538a1d46f2fe4df566ebfa2c88
黄巍 <huangwei.fps@bytedance.com>
2024-02-20 11:02:22 +0800

    feat(refly): 添加基于每页进行更新/初次安装时进行更新的内容

--- extension/popup.js
+++ extension/popup.js
@@ -1,13 +1,20 @@
 document.addEventListener("DOMContentLoaded", function () {
-  chrome.history.search({ text: "", maxResults: 10 }, function (data) {
-    console.log("data", data);
-    var historyDiv = document.getElementById("history");
-    var historyList = "<ul>";
-    data.forEach(function (page) {
-      historyList +=
-        '<li><a href="' + page.url + '">' + page.title + "</a></li>";
-    });
-    historyList += "</ul>";
-    historyDiv.innerHTML = historyList;
-  });
+  const microsecondsPerDay = 1000 * 60 * 60 * 24;
+  const daysPerMonth = 30; // 这里简单地假设一个月是30天，实际情况可能会有所不同
+  const microsecondsPerMonth = microsecondsPerDay * daysPerMonth;
+
+  chrome.history.search(
+    { text: "", startTime: microsecondsPerMonth, maxResults: 50000 },
+    function (data) {
+      console.log("data", data);
+      var historyDiv = document.getElementById("history");
+      var historyList = "<ul>";
+      data.forEach(function (page) {
+        historyList +=
+          '<li><a href="' + page.url + '">' + page.title + "</a></li>";
+      });
+      historyList += "</ul>";
+      historyDiv.innerHTML = historyList;
+    }
+  );
 });
