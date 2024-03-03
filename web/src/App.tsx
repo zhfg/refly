04a46883eef940d0caa2353e80303e9ab7879242
pftom <1043269994@qq.com>
2024-03-03 20:32:47 +0800

    feat(web): init web project for login

---
+++ web/src/App.tsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <button onClick={() => {
+        console.log('Google 登录')
+      }}>Google 登录</button>
+    </>
+  )
+}
+
+export default App
