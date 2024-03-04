f939f03fe8aeb60251fb3d0bc04f395050cabb56
mrcfps <mrc@powerformer.com>
2024-03-04 20:48:17 +0800

    feat: implement google oauth flow

--- reflyd/src/main.ts
+++ reflyd/src/main.ts
@@ -6,6 +6,7 @@ import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
   app.use(cookieParser());
 
   app.setGlobalPrefix('/v1');
--- web/src/Dashboard.tsx
+++ web/src/Dashboard.tsx
@@ -1,17 +1,60 @@
-import { useState } from 'react'
-import { useNavigate } from 'react-router-dom'
+import { useState, useEffect } from "react";
+import { useCookie } from "react-use";
+import { useNavigate } from "react-router-dom";
 
 function Dashboard() {
-    const navigate = useNavigate();
+  const navigate = useNavigate();
+  const [data, setData] = useState({});
+  const [token, updateCookie, deleteCookie] = useCookie("_refly_ai_sid");
 
+  useEffect(() => {
+    if (!token) return;
 
-    return (
+    const fetchData = async () => {
+      try {
+        const response = await fetch(
+          "http://localhost:3000/v1/auth/getUserInfo",
+          {
+            method: "GET",
+            headers: {
+              "Content-Type": "application/json",
+              Authorization: `Bearer ${token}`, // Include the JWT token in the Authorization header
+            },
+          }
+        );
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
+        }
+        const result = await response.json();
+        setData(result);
+      } catch (error) {
+        console.error("Error:", error);
+      }
+    };
+
+    fetchData();
+  }, [token]);
+
+  return (
+    <>
+      <h1>Dashboard</h1>
+
+      {token ? (
         <>
-            <div>Dashboard</div>
-            <button onClick={() => navigate('/')}>回主页</button>
-            <button onClick={() => navigate('/login')}>去登录</button>
+          <div>
+            <img src={data.avatar} alt="Avatar" />
+          </div>
+          <p>
+            你好 {data.name} ({data.email}){}
+          </p>
+          <button onClick={() => deleteCookie()}>退出登录</button>
         </>
-    )
+      ) : (
+        <button onClick={() => navigate("/login")}>去登录</button>
+      )}
+      <button onClick={() => navigate("/")}>回主页</button>
+    </>
+  );
 }
 
-export default Dashboard
+export default Dashboard;
