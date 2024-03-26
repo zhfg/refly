b9d027a0d2000054c6d67819168ad32a237539d9
pftom <1043269994@qq.com>
2024-03-26 22:17:08 +0800

    feat(web): twitter use real link

--- web/src/components/landing-page-partials/Footer.tsx
+++ web/src/components/landing-page-partials/Footer.tsx
@@ -68,18 +68,18 @@ function Footer() {
                 <ul>
                   <li className="mb-1">
                     <Link
-                      to="#"
+                      to="https://twitter.com/tuturetom"
                       className="text-gray-400 hover:text-gray-100 transition duration-150 ease-in-out">
                       Twitter
                     </Link>
                   </li>
-                  <li className="mb-1">
+                  {/* <li className="mb-1">
                     <Link
                       to="#"
                       className="text-gray-400 hover:text-gray-100 transition duration-150 ease-in-out">
                       Github
                     </Link>
-                  </li>
+                  </li> */}
                   {/*
                   <li className="mb-1">
                     <Link
@@ -155,7 +155,7 @@ function Footer() {
             <ul className="flex mb-4 md:order-1 md:ml-4 md:mb-0">
               <li>
                 <Link
-                  to="#"
+                  to="https://twitter.com/tuturetom"
                   className="flex justify-center items-center text-green-600 bg-gray-800 hover:text-gray-100 hover:bg-green-600 rounded-full transition duration-150 ease-in-out"
                   aria-label="Twitter">
                   <svg
@@ -166,7 +166,7 @@ function Footer() {
                   </svg>
                 </Link>
               </li>
-              <li className="ml-4">
+              {/* <li className="ml-4">
                 <Link
                   to="#"
                   className="flex justify-center items-center text-green-600 bg-gray-800 hover:text-gray-100 hover:bg-green-600 rounded-full transition duration-150 ease-in-out"
@@ -178,7 +178,7 @@ function Footer() {
                     <path d="M16 8.2c-4.4 0-8 3.6-8 8 0 3.5 2.3 6.5 5.5 7.6.4.1.5-.2.5-.4V22c-2.2.5-2.7-1-2.7-1-.4-.9-.9-1.2-.9-1.2-.7-.5.1-.5.1-.5.8.1 1.2.8 1.2.8.7 1.3 1.9.9 2.3.7.1-.5.3-.9.5-1.1-1.8-.2-3.6-.9-3.6-4 0-.9.3-1.6.8-2.1-.1-.2-.4-1 .1-2.1 0 0 .7-.2 2.2.8.6-.2 1.3-.3 2-.3s1.4.1 2 .3c1.5-1 2.2-.8 2.2-.8.4 1.1.2 1.9.1 2.1.5.6.8 1.3.8 2.1 0 3.1-1.9 3.7-3.7 3.9.3.4.6.9.6 1.6v2.2c0 .2.1.5.6.4 3.2-1.1 5.5-4.1 5.5-7.6-.1-4.4-3.7-8-8.1-8z" />
                   </svg>
                 </Link>
-              </li>
+              </li> */}
               {/* <li className="ml-4">
                 <Link
                   to="#"
