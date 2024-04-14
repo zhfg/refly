1227b933b31d4319e05c867fa0a56b3c16487875
mrcfps <mrc@powerformer.com>
2024-04-14 13:03:28 +0800

    fix(reflyd): set loggerservice scope to transient

--- reflyd/src/common/logger.service.ts
+++ reflyd/src/common/logger.service.ts
@@ -1,8 +1,8 @@
-import { Inject, Injectable } from '@nestjs/common';
+import { Inject, Injectable, Scope } from '@nestjs/common';
 import { PinoLogger, Params, PARAMS_PROVIDER_TOKEN } from 'nestjs-pino';
 import api from '@opentelemetry/api';
 
-@Injectable()
+@Injectable({ scope: Scope.TRANSIENT })
 export class LoggerService extends PinoLogger {
   constructor(@Inject(PARAMS_PROVIDER_TOKEN) params: Params) {
     super(params);
