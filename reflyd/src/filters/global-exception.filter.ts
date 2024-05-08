b2d68e5de3ee9effa258753f0ee3ecf017c6f5f7
mrcfps <mrc@powerformer.com>
2024-05-08 15:20:30 +0800

    fix(reflyd): global exception filter

--- reflyd/src/filters/global-exception.filter.ts
+++ reflyd/src/filters/global-exception.filter.ts
@@ -1,4 +1,11 @@
-import { ExceptionFilter, Catch, ArgumentsHost, Logger } from '@nestjs/common';
+import {
+  ExceptionFilter,
+  HttpException,
+  HttpStatus,
+  Catch,
+  ArgumentsHost,
+  Logger,
+} from '@nestjs/common';
 import { Request, Response } from 'express';
 import * as Sentry from '@sentry/node';
 
@@ -6,19 +13,25 @@ import * as Sentry from '@sentry/node';
 export class GlobalExceptionFilter implements ExceptionFilter {
   private readonly logger = new Logger(GlobalExceptionFilter.name);
 
-  catch(error: Error, host: ArgumentsHost) {
+  catch(exception: any, host: ArgumentsHost) {
     const ctx = host.switchToHttp();
     const response = ctx.getResponse<Response>();
     const request = ctx.getRequest<Request>();
 
-    // Log the error
-    Sentry.captureException(error);
-    this.logger.error(`Request: ${request.method} ${request.url}`, error);
+    // Log the error for unknown exception
+    if (!(exception instanceof HttpException)) {
+      Sentry.captureException(exception);
+      this.logger.error(`Request: ${request.method} ${request.url} err: ` + exception);
+    }
+
+    const httpStatus =
+      exception instanceof HttpException ? exception.getStatus() : HttpStatus.INTERNAL_SERVER_ERROR;
 
     // You can also customize the response sent back to the client
-    response.status(500).json({
-      statusCode: 500,
-      message: 'Internal server error',
+    response.status(httpStatus).json({
+      statusCode: httpStatus,
+      timestamp: new Date().toISOString(),
+      path: request.url,
     });
   }
 }
