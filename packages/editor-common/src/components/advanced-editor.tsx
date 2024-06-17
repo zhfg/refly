58063c0e40cea298b8c4d6e793f8f1b662f5bbfa
pftom <1043269994@qq.com>
2024-06-17 16:03:30 +0800

    feat(web): add save for Menu and make AI Selector ability with affine

--- packages/editor-common/src/components/advanced-editor.tsx
+++ packages/editor-common/src/components/advanced-editor.tsx
@@ -13,6 +13,8 @@ import { Separator } from './ui/separator';
 
 import GenerativeMenuSwitch from './generative/generative-menu-switch';
 import { TextButtons } from './selectors/text-buttons';
+import { SaveButton } from './selectors/save-buttons';
+import { AIBtnSelector } from './selectors/ai-btn-selector';
 import { suggestionItems } from './slash-command';
 
 // 样式
@@ -47,21 +49,24 @@ export const CollabEditorCommand = () => {
 
 export const CollabGenAIMenuSwitch = () => {
   const [openNode, setOpenNode] = useState(false);
+  const [openAIBtn, setOpenAIBtn] = useState(false);
   const [openColor, setOpenColor] = useState(false);
   const [openLink, setOpenLink] = useState(false);
   const [openAI, setOpenAI] = useState(false);
 
   return (
     <GenerativeMenuSwitch open={openAI} onOpenChange={setOpenAI}>
+      <AIBtnSelector open={openAIBtn} onOpenChange={setOpenAIBtn} />
       <Separator orientation="vertical" />
       <NodeSelector open={openNode} onOpenChange={setOpenNode} />
       <Separator orientation="vertical" />
-
       <LinkSelector open={openLink} onOpenChange={setOpenLink} />
       <Separator orientation="vertical" />
       <TextButtons />
       <Separator orientation="vertical" />
       <ColorSelector open={openColor} onOpenChange={setOpenColor} />
+      <Separator orientation="vertical" />
+      <SaveButton onOpenChange={setOpenAI} />
     </GenerativeMenuSwitch>
   );
 };
--- packages/editor-common/src/components/generative/generative-menu-switch.tsx
+++ packages/editor-common/src/components/generative/generative-menu-switch.tsx
@@ -2,9 +2,6 @@ import { EditorBubble, useEditor } from '@refly-packages/editor-core/components'
 import { removeAIHighlight } from '@refly-packages/editor-core/extensions';
 import {} from '@refly-packages/editor-core/plugins';
 import { Fragment, type ReactNode, useEffect } from 'react';
-import { Button } from '../ui/button';
-import Magic from '../ui/icons/magic';
-import { AISelector } from './ai-selector';
 
 interface GenerativeMenuSwitchProps {
   children: ReactNode;
@@ -28,21 +25,7 @@ const GenerativeMenuSwitch = ({ children, open, onOpenChange }: GenerativeMenuSw
       }}
       className="flex w-fit max-w-[90vw] overflow-hidden rounded-md border border-muted bg-background shadow-xl"
     >
-      {open && <AISelector open={open} onOpenChange={onOpenChange} />}
-      {!open && (
-        <Fragment>
-          <Button
-            className="gap-1 rounded-none text-purple-500"
-            variant="ghost"
-            onClick={() => onOpenChange(true)}
-            size="sm"
-          >
-            <Magic className="h-5 w-5" />
-            Ask AI
-          </Button>
-          {children}
-        </Fragment>
-      )}
+      {!open && <Fragment>{children}</Fragment>}
     </EditorBubble>
   );
 };
