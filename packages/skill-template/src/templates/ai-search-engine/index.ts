93a874c09982f2307bf94e18386df6ad9e18d4e3
mrcfps <mrc@powerformer.com>
2024-06-25 10:22:42 +0800

    feat(skill-template): implement basic engine injection for skills

--- packages/skill-template/src/templates/ai-search-engine/index.ts
+++ packages/skill-template/src/templates/ai-search-engine/index.ts
@@ -2,18 +2,20 @@ import { ToolNode } from '@langchain/langgraph/prebuilt';
 import { ChatPromptTemplate, MessagesPlaceholder } from '@langchain/core/prompts';
 import { StringOutputParser } from '@langchain/core/output_parsers';
 import { Document } from '@langchain/core/documents';
+import { Tool } from '@langchain/core/tools';
 import { ChatOpenAI, OpenAI } from '@langchain/openai';
 
 import { AIMessage, BaseMessage } from '@langchain/core/messages';
-import { START, END, MessageGraph, StateGraphArgs, StateGraph } from '@langchain/langgraph';
+import { START, END, StateGraphArgs, StateGraph } from '@langchain/langgraph';
 
 // tools
 import { SerperSearch } from '../../tools/serper-online-search';
 // types
-import { Source } from '@refly/openapi-schema';
 import { SystemMessage } from '@langchain/core/messages';
 import { HumanMessage } from '@langchain/core/messages';
-import { RunnableConfig } from '@langchain/core/runnables';
+import { Runnable, RunnableConfig } from '@langchain/core/runnables';
+import { BaseSkill } from 'src/base';
+import SkillEngine from 'src/engine';
 
 export enum LOCALE {
   ZH_CN = 'zh-CN',
@@ -50,10 +52,68 @@ const graphState: StateGraphArgs<GraphState>['channels'] = {
   },
 };
 
-export const getContextualQuestion = async (state: GraphState): Promise<Partial<GraphState>> => {
-  const { locale, userQuery, messages } = state;
+class OnlineSearchSkill extends BaseSkill {
+  private tools: Tool[];
 
-  const getSystemPrompt = (locale: LOCALE) => `
+  constructor(protected engine: SkillEngine) {
+    super(engine);
+
+    this.tools = [
+      new SerperSearch({
+        searchOptions: {
+          maxResults: 8,
+          locale: 'en' as LOCALE,
+        },
+        engine: this.engine,
+      }),
+    ];
+  }
+
+  async callModel(state: GraphState, config?: RunnableConfig) {
+    const boundModel = new ChatOpenAI({ model: 'gpt-3.5-turbo', openAIApiKey: process.env.OPENAI_API_KEY }).bindTools(
+      this.tools,
+    );
+
+    // For versions of @langchain/core < 0.2.3, you must call `.stream()`
+    // and aggregate the message from chunks instead of calling `.invoke()`.
+
+    const { userQuery, contextualUserQuery, locale, messages } = state;
+    const lastMessage = messages[messages.length - 1];
+    const function_call = lastMessage.additional_kwargs.function_call;
+    const onlineReleventDocs = JSON.parse(function_call.arguments);
+
+    const contextToCitationText = onlineReleventDocs
+      .map((item, index) => `[[citation:${index + 1}]] ${item?.pageContent}`)
+      .join('\n\n');
+
+    const getSystemPrompt = (
+      context: string,
+    ) => `You are a large language AI assistant built by Refly AI. You are given a user question, and please write **detail** and accurate answer to the question. You will be given a set of related contexts to the question, each starting with a reference number like [[citation:x]], where x is a number. Please use the context and cite the context at the end of each sentence if applicable.
+    
+    Your answer must be correct, accurate and written by an expert using an unbiased and professional tone. Please limit to 1024 tokens. Do not give any information that is not related to the question, and do not repeat. Say "information is missing on" followed by the related topic, if the given context do not provide sufficient information.
+    
+    Please cite the contexts with the reference numbers, in the format [citation:x]. If a sentence comes from multiple contexts, please list all applicable citations, like [citation:3][citation:5]. Other than code and specific names and citations, your answer must be written in the same language as the question.
+    
+    Here are the set of contexts:
+    
+    ===
+    ${context}
+    ===
+    
+    Remember, don't blindly repeat the contexts verbatim. And here is the user question:`;
+
+    const responseMessage = await boundModel.invoke([
+      new SystemMessage(getSystemPrompt(contextToCitationText)),
+      new HumanMessage(`The user's query is ${userQuery}, please output answer in locale's ${locale} language:`),
+    ]);
+
+    return { messages: [responseMessage] };
+  }
+
+  async getContextualQuestion(state: GraphState) {
+    const { locale, userQuery, messages } = state;
+
+    const getSystemPrompt = (locale: LOCALE) => `
 ## Target
 Given a chat history and the latest user question
 which might reference context in the chat history, formulate a standalone question
@@ -64,22 +124,43 @@ just reformulate it if needed and otherwise return it as is.
 **Please output answer in ${locale} language.**
 `;
 
-  // 构建总结的 Prompt，将 question + chatHistory 总结成
-  const contextualizeQPrompt = ChatPromptTemplate.fromMessages([
-    ['system', getSystemPrompt(locale)],
-    new MessagesPlaceholder('chatHistory'),
-    ['human', `The user's question is {question}, please output answer in ${locale} language:`],
-  ]);
-  const llm = new OpenAI({ modelName: 'gpt-3.5-turbo', temperature: 0, openAIApiKey: process.env.OPENAI_API_KEY });
-  const contextualizeQChain = contextualizeQPrompt.pipe(llm).pipe(new StringOutputParser());
+    // 构建总结的 Prompt，将 question + chatHistory 总结成
+    const contextualizeQPrompt = ChatPromptTemplate.fromMessages([
+      ['system', getSystemPrompt(locale)],
+      new MessagesPlaceholder('chatHistory'),
+      ['human', `The user's question is {question}, please output answer in ${locale} language:`],
+    ]);
+    const llm = new OpenAI({ modelName: 'gpt-3.5-turbo', temperature: 0, openAIApiKey: process.env.OPENAI_API_KEY });
+    const contextualizeQChain = contextualizeQPrompt.pipe(llm).pipe(new StringOutputParser());
 
-  const contextualUserQuery = await contextualizeQChain.invoke({
-    question: userQuery,
-    chatHistory: messages,
-  });
+    const contextualUserQuery = await contextualizeQChain.invoke({
+      question: userQuery,
+      chatHistory: messages,
+    });
 
-  return { contextualUserQuery };
-};
+    return { contextualUserQuery };
+  }
+
+  toRunnable(): Runnable<any, any, RunnableConfig> {
+    const toolNode = new ToolNode<GraphState>(this.tools);
+
+    const workflow = new StateGraph<GraphState>({
+      channels: graphState,
+    })
+      .addNode('agent', this.callModel)
+      .addNode('action', toolNode)
+      .addNode('contextualUserQuery', this.getContextualQuestion);
+
+    workflow.addConditionalEdges(START, shouldMakeContextualUserQuery);
+    workflow.addEdge('contextualUserQuery', 'agent');
+    // Conditional agent -> action OR agent -> END
+    workflow.addConditionalEdges('agent', shouldContinue);
+    // Always transition `action` -> `agent`
+    workflow.addEdge('action', 'agent');
+
+    return workflow.compile();
+  }
+}
 
 function shouldMakeContextualUserQuery(state: GraphState): 'contextualUserQuery' | 'agent' {
   const { messages } = state;
@@ -104,75 +185,4 @@ function shouldContinue(state: GraphState): 'action' | typeof END {
   }
 }
 
-// Define a new graph
-
-const tools = [
-  new SerperSearch({
-    searchOptions: {
-      maxResults: 8,
-      locale: 'en' as LOCALE,
-    },
-  }),
-];
-const boundModel = new ChatOpenAI({ model: 'gpt-3.5-turbo', openAIApiKey: process.env.OPENAI_API_KEY }).bindTools(
-  tools,
-);
-
-const callModel = async (state: GraphState, config?: RunnableConfig) => {
-  // For versions of @langchain/core < 0.2.3, you must call `.stream()`
-  // and aggregate the message from chunks instead of calling `.invoke()`.
-
-  const { userQuery, contextualUserQuery, locale, messages } = state;
-  const lastMessage = messages[messages.length - 1];
-  const function_call = lastMessage.additional_kwargs.function_call;
-  const onlineReleventDocs = JSON.parse(function_call.arguments);
-
-  const contextToCitationText = onlineReleventDocs
-    .map((item, index) => `[[citation:${index + 1}]] ${item?.pageContent}`)
-    .join('\n\n');
-
-  const getSystemPrompt = (
-    context: string,
-  ) => `You are a large language AI assistant built by Refly AI. You are given a user question, and please write **detail** and accurate answer to the question. You will be given a set of related contexts to the question, each starting with a reference number like [[citation:x]], where x is a number. Please use the context and cite the context at the end of each sentence if applicable.
-
-Your answer must be correct, accurate and written by an expert using an unbiased and professional tone. Please limit to 1024 tokens. Do not give any information that is not related to the question, and do not repeat. Say "information is missing on" followed by the related topic, if the given context do not provide sufficient information.
-
-Please cite the contexts with the reference numbers, in the format [citation:x]. If a sentence comes from multiple contexts, please list all applicable citations, like [citation:3][citation:5]. Other than code and specific names and citations, your answer must be written in the same language as the question.
-
-Here are the set of contexts:
-
-===
-${context}
-===
-
-Remember, don't blindly repeat the contexts verbatim. And here is the user question:`;
-
-  const responseMessage = await boundModel.invoke([
-    new SystemMessage(getSystemPrompt(contextToCitationText)),
-    new HumanMessage(`The user's query is ${userQuery}, please output answer in locale's ${locale} language:`),
-  ]);
-
-  return { messages: [responseMessage] };
-};
-
-const toolNode = new ToolNode<GraphState>(tools);
-
-const workflow = new StateGraph<GraphState>({
-  channels: graphState,
-})
-  .addNode('agent', callModel)
-  .addNode('action', toolNode)
-  .addNode('contextualUserQuery', getContextualQuestion);
-
-workflow.addConditionalEdges(START, shouldMakeContextualUserQuery);
-workflow.addEdge('contextualUserQuery', 'agent');
-// Conditional agent -> action OR agent -> END
-workflow.addConditionalEdges('agent', shouldContinue);
-// Always transition `action` -> `agent`
-workflow.addEdge('action', 'agent');
-
-// const memory = SqliteSaver.fromConnString(':memory:'); // Here we only save in-memory
-
-// Setting the interrupt means that any time an action is called, the machine will stop
-// export const SearchAndAddResource = workflow.compile({ checkpointer: memory, interruptBefore: ['action'] });
-export const SearchAndAddResource = workflow.compile();
+export default OnlineSearchSkill;
