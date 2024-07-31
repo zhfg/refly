9697ac3e1144de11d0dfc9567d0180b1cbd8eac2
CH <ch@refly.ai>
2024-07-31 22:36:52 +0800

    feat(ai-workspace-common): add NewButton and search to workspace header

--- packages/ai-workspace-common/src/components/search-quick-open-btn/index.tsx
+++ packages/ai-workspace-common/src/components/search-quick-open-btn/index.tsx
@@ -3,26 +3,26 @@ import { reflyEnv } from '@refly-packages/ai-workspace-common/utils/env';
 
 import './index.scss';
 import { useTranslation } from 'react-i18next';
-import { useSearchStore } from '@refly-packages/ai-workspace-common/stores/search';
 import { bigSearchQuickOpenEmitter } from '@refly-packages/ai-workspace-common/utils/event-emitter/big-search-quick-open';
+import classNames from 'classnames';
 
-export const SearchQuickOpenBtn = () => {
-  // stores
-  // const quickSearchStateStore = useQuickSearchStateStore();
-  const searchStore = useSearchStore();
+interface SearchQuickOpenBtnProps extends React.ComponentProps<'div'> {
+  placeholder?: string;
+}
+export const SearchQuickOpenBtn = (props: SearchQuickOpenBtnProps) => {
+  const { placeholder, ...divProps } = props;
 
   const { t } = useTranslation();
 
   return (
-    <div className="search-quick-open-container">
+    <div {...divProps} className={classNames('search-quick-open-container', divProps.className)}>
       <div
         className="search-quick-open-input"
         onClick={() => {
-          // quickSearchStateStore.setVisible(true);
           bigSearchQuickOpenEmitter.emit('openSearch');
         }}
       >
-        <div className="search-quick-open-text">{t('loggedHomePage.newThreadText')}</div>
+        <div className="search-quick-open-text">{t(`${placeholder || 'loggedHomePage.newThreadText'}`)}</div>
         <div className="search-quick-open-shortcuts">
           <div className="search-quick-open-shortcut-key">{reflyEnv.getOsType() === 'OSX' ? '⌘' : 'ctrl'}</div>
           <div className="search-quick-open-shortcut-key search-quick-open-shortcut-key__right">K</div>
--- packages/ai-workspace-common/src/components/workspace/content-panel/index.tsx
+++ packages/ai-workspace-common/src/components/workspace/content-panel/index.tsx
@@ -1,11 +1,15 @@
 import { useRef, useState } from 'react';
-import { Affix, Radio } from '@arco-design/web-react';
+import { Affix, Radio, Dropdown, Menu, Divider } from '@arco-design/web-react';
+import { IconDown } from '@arco-design/web-react/icon';
 import { useTranslation } from 'react-i18next';
 
 import { WorkSpaceSearch } from '../work-space-search';
 import { ResourceList } from '@refly-packages/ai-workspace-common/components/workspace/resource-list';
 import { NoteList } from '@refly-packages/ai-workspace-common/components/workspace/note-list';
 import { KnowledgeBaseList } from '@refly-packages/ai-workspace-common/components/knowledge-base-list';
+import { SearchQuickOpenBtn } from '@refly-packages/ai-workspace-common/components/search-quick-open-btn';
+import { useImportResourceStore } from '@refly/ai-workspace-common/stores/import-resource';
+import { useAINote } from '@refly-packages/ai-workspace-common/hooks/use-ai-note';
 
 import './index.scss';
 import classNames from 'classnames';
@@ -14,48 +18,107 @@ const RadioGroup = Radio.Group;
 
 const Content = (props: { val: string }) => {
   switch (props.val) {
-    case 'knowledge-resource':
+    case 'resource':
       return <ResourceList />;
-    case 'knowledge-notes':
+    case 'note':
       return <NoteList />;
-    case 'knowledge-collection':
+    case 'collection':
       return <KnowledgeBaseList />;
     default:
       return <ResourceList />;
   }
 };
 
-const ContentHeader = (props: { setVal: (val: string) => void; hitTop: boolean }) => {
+const NewFileDropList = (props: { handleCreateButtonClick: (type: string) => void }) => {
+  const { t } = useTranslation();
+  return (
+    <Menu>
+      {['resource', 'note', 'collection'].map((item) => {
+        return (
+          <Menu.Item key={item} onClick={() => props.handleCreateButtonClick(item)}>
+            {t(`workspace.contentPanel.newButton.${item}`)}
+          </Menu.Item>
+        );
+      })}
+    </Menu>
+  );
+};
+
+const NewFileButton = (props: { val: string }) => {
+  const { t } = useTranslation();
+  const importResourceStore = useImportResourceStore();
+  const { handleInitEmptyNote } = useAINote();
+
+  const handleCreateButtonClick = (type: string) => {
+    if (type === 'note') {
+      handleInitEmptyNote('');
+    }
+    if (type === 'resource') {
+      importResourceStore.setImportResourceModalVisible(true);
+    }
+    if (type === 'collection') {
+      // TODO: '创建知识库'
+    }
+  };
+
+  return (
+    <Dropdown.Button
+      type="primary"
+      droplist={<NewFileDropList handleCreateButtonClick={(type) => handleCreateButtonClick(type)} />}
+      icon={<IconDown />}
+    >
+      <div onClick={() => handleCreateButtonClick(props.val)}>{t(`workspace.contentPanel.newButton.${props.val}`)}</div>
+    </Dropdown.Button>
+  );
+};
+
+const ContentHeader = (props: { setVal: (val: string) => void; hitTop: boolean; val: string }) => {
   const { t } = useTranslation();
   const { setVal, hitTop } = props;
 
   return (
-    <div className={classNames('content-panel-header', { 'content-panel-header-hit-top': hitTop }, 'h-16 pt-3')}>
-      <RadioGroup
-        type="button"
-        size="large"
-        className="content-panel-radio-group"
-        defaultValue="knowledge-resource"
-        onChange={(val) => setVal(val)}
-      >
-        <Radio value="knowledge-resource">{t('workspace.contentPanel.tabPanel.resource')}</Radio>
-        <Radio value="knowledge-notes">{t('workspace.contentPanel.tabPanel.note')}</Radio>
-        <Radio value="knowledge-collection">{t('workspace.contentPanel.tabPanel.collection')}</Radio>
-      </RadioGroup>
+    <div
+      className={classNames(
+        'content-panel-header',
+        { 'content-panel-header-hit-top': hitTop },
+        'h-16 pt-3 pb-3 flex justify-between items-center',
+      )}
+    >
+      <div className="flex items-center">
+        <RadioGroup
+          type="button"
+          size="large"
+          className="content-panel-radio-group"
+          defaultValue="resource"
+          onChange={(val) => setVal(val)}
+        >
+          <Radio value="resource">{t('workspace.contentPanel.tabPanel.resource')}</Radio>
+          <Radio value="note">{t('workspace.contentPanel.tabPanel.note')}</Radio>
+          <Radio value="collection">{t('workspace.contentPanel.tabPanel.collection')}</Radio>
+        </RadioGroup>
+        {hitTop && (
+          <SearchQuickOpenBtn
+            className="work-space-top-search"
+            placeholder="loggedHomePage.newThreadTextForWorkSpace"
+          />
+        )}
+      </div>
+
+      <NewFileButton val={props.val}></NewFileButton>
     </div>
   );
 };
 
 export const ContentPanel = () => {
   const ref = useRef();
-  const [val, setVal] = useState('knowledge-resource');
+  const [val, setVal] = useState('resource');
   const [hitTop, setHitTop] = useState(false);
 
   return (
     <div className="content-panel-container" ref={ref}>
       <WorkSpaceSearch />
       <Affix offsetTop={0} target={() => ref.current} onChange={setHitTop}>
-        <ContentHeader setVal={setVal} hitTop={hitTop} />
+        <ContentHeader val={val} setVal={setVal} hitTop={hitTop} />
       </Affix>
       <Content val={val} />
     </div>
