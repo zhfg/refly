48242b6cce0e7f48e1a87bbbe8d29adb965ef9c9
pftom <1043269994@qq.com>
2024-08-01 20:13:42 +0800

    feat(web): optimize skill management resize logic

--- packages/ai-workspace-common/src/components/knowledge-base/copilot/skill-display/index.tsx
+++ packages/ai-workspace-common/src/components/knowledge-base/copilot/skill-display/index.tsx
@@ -1,12 +1,32 @@
 import { IconSettings } from '@arco-design/web-react/icon';
+import { useResizeBox } from '@refly-packages/ai-workspace-common/hooks/use-resize-box';
 import { useSkillStore } from '@refly-packages/ai-workspace-common/stores/skill';
+import { getPopupContainer } from '@refly-packages/ai-workspace-common/utils/ui';
 
 export const SkillDisplay = () => {
   const skillStore = useSkillStore();
 
+  const [containCnt] = useResizeBox({
+    getGroupSelector: () => {
+      const container = getPopupContainer();
+      const elem = container.querySelector('.skill-container');
+
+      return elem as HTMLElement;
+    },
+    getResizeSelector: () => {
+      const container = getPopupContainer();
+      const elems = container.querySelectorAll('.skill-item') as NodeListOf<HTMLElement>;
+
+      return elems;
+    },
+    initialContainCnt: 3,
+    paddingSize: 0,
+    placeholderWidth: 100,
+  });
+
   return (
     <div className="skill-container">
-      {skillStore?.skillInstances?.map((item, index) => (
+      {skillStore?.skillInstances?.slice(0, containCnt).map((item, index) => (
         <div
           key={index}
           className="skill-item"
