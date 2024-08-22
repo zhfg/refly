4322bc4bb9f31c599db155715e3929f880241890
mrcfps <mrc@powerformer.com>
2024-08-22 21:08:39 +0800

    refactor(api): make MinioService support dynamic bucket configuration

--- apps/api/src/common/minio.service.ts
+++ apps/api/src/common/minio.service.ts
@@ -1,49 +1,38 @@
-import { Injectable, Logger, OnModuleInit } from '@nestjs/common';
-import { ConfigService } from '@nestjs/config';
-import { Client } from 'minio';
-import { streamToBuffer } from '../utils';
+import { MinioConfig } from '@/config/app.config';
+import { Inject, Injectable, Logger, OnModuleInit } from '@nestjs/common';
+import { Client as MinioClient } from 'minio';
+
+// Internal object storage, which is private and only consumed by api server
+export const MINIO_INTERNAL = 'minio-internal';
+
+// External object storage, which is typically public and allow anonymous access
+export const MINIO_EXTERNAL = 'minio-external';
 
 @Injectable()
-export class MinioService implements OnModuleInit {
-  private client: Client;
-  private bucketName: string;
+export class MinioService extends MinioClient implements OnModuleInit {
   private readonly logger = new Logger(MinioService.name);
 
-  constructor(private configService: ConfigService) {
-    this.client = new Client({
-      endPoint: configService.getOrThrow('minio.endpoint'),
-      port: configService.getOrThrow('minio.port'),
-      useSSL: configService.getOrThrow('minio.useSSL') === 'true',
-      accessKey: configService.getOrThrow('minio.accessKey'),
-      secretKey: configService.getOrThrow('minio.secretKey'),
+  constructor(@Inject('MINIO_CONFIG') private config: MinioConfig) {
+    super({
+      endPoint: config.endPoint,
+      port: config.port,
+      useSSL: config.useSSL,
+      accessKey: config.accessKey,
+      secretKey: config.secretKey,
     });
-    this.bucketName = configService.getOrThrow('minio.weblinkBucket');
-  }
-
-  async ensureBucketExists(bucket: string) {
-    const exists = await this.client.bucketExists(bucket);
-    if (exists) {
-      this.logger.log('Bucket ' + bucket + ' exists');
-    } else {
-      await this.client.makeBucket(bucket);
-      this.logger.log('Bucket ' + bucket + ' created');
-    }
   }
 
   async onModuleInit() {
-    await this.ensureBucketExists(this.configService.getOrThrow('minio.weblinkBucket'));
+    const exists = await this.bucketExists(this.config.bucket);
+    if (exists) {
+      this.logger.log('Bucket ' + this.config.bucket + ' exists');
+    } else {
+      await this.makeBucket(this.config.bucket);
+      this.logger.log('Bucket ' + this.config.bucket + ' created');
+    }
   }
 
-  async downloadData(key: string) {
-    const stream = await this.client.getObject(this.bucketName, key);
-    return streamToBuffer(stream);
-  }
-
-  async uploadData(key: string, data: string | Buffer) {
-    return this.client.putObject(this.bucketName, key, data);
-  }
-
-  async removeObject(key: string) {
-    return this.client.removeObject(this.bucketName, key);
+  bucketName() {
+    return this.config.bucket;
   }
 }
