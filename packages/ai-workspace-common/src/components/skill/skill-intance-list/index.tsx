104b22214a968286fbf5b59bb21a91e846bdf01a
pftom <1043269994@qq.com>
2024-08-28 15:36:01 +0800

    feat(web): support search for skill management

--- packages/ai-workspace-common/src/components/skill/skill-intance-list/index.tsx
+++ packages/ai-workspace-common/src/components/skill/skill-intance-list/index.tsx
@@ -1,4 +1,4 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useTranslation } from 'react-i18next';
 // components
 import { SkillItem } from '@refly-packages/ai-workspace-common/components/skill/skill-management/skill-item';
@@ -10,13 +10,18 @@ import { useFetchDataList } from '@refly-packages/ai-workspace-common/hooks/use-
 import { SkillInstance } from '@refly/openapi-schema';
 
 import { ScrollLoading } from '@refly-packages/ai-workspace-common/components/workspace/scroll-loading';
-import { List, Empty } from '@arco-design/web-react';
+import { List, Empty, Button, Input } from '@arco-design/web-react';
+import { IconArrowRight, IconSearch } from '@arco-design/web-react/icon';
+import { useSearchableList } from '@refly-packages/ai-workspace-common/components/use-searchable-list';
+import { useNavigate } from 'react-router-dom';
+import { useSkillStore } from '@refly-packages/ai-workspace-common/stores/skill';
 
 interface SkillInstanceListProps {
   canGoDetail?: boolean;
 }
 export const SkillInstanceList = (props: SkillInstanceListProps) => {
   const { t } = useTranslation();
+  const setSkillManagerModalVisible = useSkillStore((state) => state.setSkillManagerModalVisible);
   const { dataList, loadMore, setDataList, hasMore, isRequesting, reload } = useFetchDataList({
     fetchData: async (queryPayload) => {
       const res = await getClient().listSkillInstances({
@@ -26,52 +31,92 @@ export const SkillInstanceList = (props: SkillInstanceListProps) => {
     },
     pageSize: 12,
   });
+  const navigate = useNavigate();
+
+  // support search by displayName
+  const [searchVal, setSearchVal] = useState('');
+  const [skillList, setSkillList, filter] = useSearchableList<SkillInstance>('displayName' as keyof SkillInstance, {
+    debounce: true,
+    delay: 300,
+  });
+
+  const goSkillList = () => {
+    setSkillManagerModalVisible(false);
+    navigate('/skill?tab=template');
+  };
+
+  const handleChange = (val: string) => {
+    filter(val);
+    setSearchVal(val);
+  };
 
   useEffect(() => {
     loadMore();
   }, []);
+  useEffect(() => {
+    setSkillList(dataList);
+  }, [dataList?.length]);
 
   if (dataList.length === 0 && !isRequesting) {
     return <Empty description={t('skill.skillDetail.emptyInstances')} />;
   }
   return (
-    <List
-      className="skill-instance-list"
-      grid={{
-        sm: 42,
-        md: 16,
-        lg: 10,
-        xl: 8,
-      }}
-      wrapperStyle={{ width: '100%' }}
-      bordered={false}
-      pagination={false}
-      dataSource={dataList}
-      loading={isRequesting}
-      scrollLoading={<ScrollLoading isRequesting={isRequesting} hasMore={hasMore} loadMore={loadMore} />}
-      render={(item: SkillInstance, key) => (
-        <List.Item
-          key={key}
-          style={{
-            padding: '0',
-            width: '100%',
-          }}
-          className="skill-instance-list__item"
-          actionLayout="vertical"
-          onClick={() => {}}
-        >
-          <SkillItem
-            itemKey={key}
-            data={item}
-            source="instance"
-            canGoDetail={props.canGoDetail}
-            isInstalled={true}
-            showExecute={true}
-            refreshList={reload}
-            postDeleteList={(item: SkillInstance) => setDataList(dataList.filter((n) => n.skillId !== item.skillId))}
+    <>
+      <div className="skill-management-modal__top-container">
+        <div className="skill-search-container">
+          <Input
+            placeholder={t('skill.skillManagement.searchPlaceholder') || ''}
+            allowClear
+            className="knowledge-base-directory-search"
+            style={{ height: 32, borderRadius: '8px' }}
+            value={searchVal}
+            prefix={<IconSearch />}
+            onChange={handleChange}
           />
-        </List.Item>
-      )}
-    />
+        </div>
+        <Button type="primary" style={{ borderRadius: 8, height: 32 }} onClick={goSkillList}>
+          <IconArrowRight />
+          {t('skill.tab.skillTemplate')}
+        </Button>
+      </div>
+      <List
+        className="skill-instance-list"
+        grid={{
+          sm: 42,
+          md: 16,
+          lg: 10,
+          xl: 8,
+        }}
+        wrapperStyle={{ width: '100%' }}
+        bordered={false}
+        pagination={false}
+        dataSource={skillList}
+        loading={isRequesting}
+        scrollLoading={<ScrollLoading isRequesting={isRequesting} hasMore={hasMore} loadMore={loadMore} />}
+        render={(item: SkillInstance, key) => (
+          <List.Item
+            key={key}
+            style={{
+              padding: '0',
+              width: '100%',
+            }}
+            className="skill-instance-list__item"
+            actionLayout="vertical"
+            onClick={() => {}}
+          >
+            <SkillItem
+              itemKey={key}
+              data={item}
+              source="instance"
+              canGoDetail={props.canGoDetail}
+              isInstalled={true}
+              showExecute={true}
+              refreshList={reload}
+              postDeleteList={(item: SkillInstance) => setDataList(dataList.filter((n) => n.skillId !== item.skillId))}
+            />
+          </List.Item>
+        )}
+      />
+    </>
   );
 };
--- packages/ai-workspace-common/src/components/skill/skill-management-modal/index.tsx
+++ packages/ai-workspace-common/src/components/skill/skill-management-modal/index.tsx
@@ -1,27 +1,17 @@
 import { useSkillStore } from '@refly-packages/ai-workspace-common/stores/skill';
-import { Modal, Button } from '@arco-design/web-react';
-import { IconArrowRight } from '@arco-design/web-react/icon';
+import { Modal } from '@arco-design/web-react';
 
 // 组件
 import { SkillInstanceList } from '@refly-packages/ai-workspace-common/components/skill/skill-intance-list';
 // 样式
 import './index.scss';
 import { getPopupContainer } from '@refly-packages/ai-workspace-common/utils/ui';
-import { useTranslation } from 'react-i18next';
-import { useNavigate } from '@refly-packages/ai-workspace-common/utils/router';
 
 export const SkillManagementModal = (props: any) => {
   const skillStore = useSkillStore();
-  const { t } = useTranslation();
-  const navigate = useNavigate();
 
   const onCancel = () => {
     skillStore.setSkillManagerModalVisible(false);
-  };
-
-  const goSkillList = () => {
-    onCancel();
-    navigate('/skill?tab=template');
   };
 
   return (
@@ -40,12 +30,6 @@ export const SkillManagementModal = (props: any) => {
       escToExit
     >
       <div className="skill-management-modal">
-        <div className="skill-management-modal__top-btn">
-          <Button type="primary" style={{ borderRadius: 8 }} onClick={goSkillList}>
-            <IconArrowRight />
-            {t('skill.tab.skillTemplate')}
-          </Button>
-        </div>
         <SkillInstanceList />
       </div>
     </Modal>
