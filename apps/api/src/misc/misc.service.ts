ce1fbf36d3ab1b52de617cbb7ea5516657fb4d98
mrcfps <mrc@powerformer.com>
2024-09-23 15:50:09 +0800

    fix(api): static file url error

--- apps/api/src/misc/misc.service.ts
+++ apps/api/src/misc/misc.service.ts
@@ -140,7 +140,7 @@ export class MiscService {
     });
 
     return {
-      url: `${this.config.get('staticEndpoint')}${objectKey}`,
+      url: `${this.config.get('staticEndpoint')}${storageKey}`,
     };
   }
 
