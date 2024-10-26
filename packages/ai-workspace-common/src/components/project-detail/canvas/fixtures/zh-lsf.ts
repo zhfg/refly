39ed87559222b0436e20b5c361a6737d69a13113
mrcfps <mrc@powerformer.com>
2024-10-26 22:15:01 +0800

    feat(ai-workspace-common): implement first working version of stream token processor

---
+++ packages/ai-workspace-common/src/components/project-detail/canvas/fixtures/zh-lsf.ts
@@ -0,0 +1,1469 @@
+export const zhLsfContent = [
+  {
+    content: '\n',
+  },
+  {
+    content: '#',
+  },
+  {
+    content: ' 螺',
+  },
+  {
+    content: '蛳',
+  },
+  {
+    content: '粉',
+  },
+  {
+    content: '制作',
+  },
+  {
+    content: '过程',
+  },
+  {
+    content: '指南',
+  },
+  {
+    content: '\n\n',
+  },
+  {
+    content: '螺',
+  },
+  {
+    content: '蛳',
+  },
+  {
+    content: '粉',
+  },
+  {
+    content: '是一',
+  },
+  {
+    content: '道',
+  },
+  {
+    content: '独',
+  },
+  {
+    content: '特',
+  },
+  {
+    content: '的',
+  },
+  {
+    content: '广西',
+  },
+  {
+    content: '小',
+  },
+  {
+    content: '吃',
+  },
+  {
+    content: '，以',
+  },
+  {
+    content: '其',
+  },
+  {
+    content: '酸',
+  },
+  {
+    content: '辣',
+  },
+  {
+    content: '鲜',
+  },
+  {
+    content: '香',
+  },
+  {
+    content: '的',
+  },
+  {
+    content: '口',
+  },
+  {
+    content: '味',
+  },
+  {
+    content: '而',
+  },
+  {
+    content: '闻',
+  },
+  {
+    content: '名',
+  },
+  {
+    content: '。',
+  },
+  {
+    content: '以下',
+  },
+  {
+    content: '是',
+  },
+  {
+    content: '制作',
+  },
+  {
+    content: '螺',
+  },
+  {
+    content: '蛳',
+  },
+  {
+    content: '粉',
+  },
+  {
+    content: '的',
+  },
+  {
+    content: '详细',
+  },
+  {
+    content: '步骤',
+  },
+  {
+    content: '。\n\n',
+  },
+  {
+    content: '##',
+  },
+  {
+    content: ' 材',
+  },
+  {
+    content: '料',
+  },
+  {
+    content: '准备',
+  },
+  {
+    content: '\n',
+  },
+  {
+    content: '在',
+  },
+  {
+    content: '开始',
+  },
+  {
+    content: '制作',
+  },
+  {
+    content: '之前',
+  },
+  {
+    content: '，',
+  },
+  {
+    content: '您',
+  },
+  {
+    content: '需要',
+  },
+  {
+    content: '准备',
+  },
+  {
+    content: '以下',
+  },
+  {
+    content: '材料',
+  },
+  {
+    content: '：\n',
+  },
+  {
+    content: '-',
+  },
+  {
+    content: ' 螺',
+  },
+  {
+    content: '蛳',
+  },
+  {
+    content: '粉',
+  },
+  {
+    content: '（',
+  },
+  {
+    content: '米',
+  },
+  {
+    content: '粉',
+  },
+  {
+    content: '）\n',
+  },
+  {
+    content: '-',
+  },
+  {
+    content: ' 螺',
+  },
+  {
+    content: '蛳',
+  },
+  {
+    content: '（',
+  },
+  {
+    content: '新',
+  },
+  {
+    content: '鲜',
+  },
+  {
+    content: '或',
+  },
+  {
+    content: '罐',
+  },
+  {
+    content: '装',
+  },
+  {
+    content: '）\n',
+  },
+  {
+    content: '-',
+  },
+  {
+    content: ' 酸',
+  },
+  {
+    content: '笋',
+  },
+  {
+    content: '\n',
+  },
+  {
+    content: '-',
+  },
+  {
+    content: ' 花',
+  },
+  {
+    content: '生',
+  },
+  {
+    content: '\n',
+  },
+  {
+    content: '-',
+  },
+  {
+    content: ' 香',
+  },
+  {
+    content: '菜',
+  },
+  {
+    content: '\n',
+  },
+  {
+    content: '-',
+  },
+  {
+    content: ' 青',
+  },
+  {
+    content: '蒜',
+  },
+  {
+    content: '\n',
+  },
+  {
+    content: '-',
+  },
+  {
+    content: ' 辣',
+  },
+  {
+    content: '椒',
+  },
+  {
+    content: '油',
+  },
+  {
+    content: '\n',
+  },
+  {
+    content: '-',
+  },
+  {
+    content: ' 醋',
+  },
+  {
+    content: '\n',
+  },
+  {
+    content: '-',
+  },
+  {
+    content: ' 盐',
+  },
+  {
+    content: '\n',
+  },
+  {
+    content: '-',
+  },
+  {
+    content: ' 鸡',
+  },
+  {
+    content: '精',
+  },
+  {
+    content: '\n',
+  },
+  {
+    content: '-',
+  },
+  {
+    content: ' 高',
+  },
+  {
+    content: '汤',
+  },
+  {
+    content: '（',
+  },
+  {
+    content: '可',
+  },
+  {
+    content: '选',
+  },
+  {
+    content: '）\n\n',
+  },
+  {
+    content: '##',
+  },
+  {
+    content: ' 制',
+  },
+  {
+    content: '作',
+  },
+  {
+    content: '步骤',
+  },
+  {
+    content: '\n\n',
+  },
+  {
+    content: '1',
+  },
+  {
+    content: '.',
+  },
+  {
+    content: ' **',
+  },
+  {
+    content: '准备',
+  },
+  {
+    content: '螺',
+  },
+  {
+    content: '蛳',
+  },
+  {
+    content: '**\n',
+  },
+  {
+    content: '  ',
+  },
+  {
+    content: ' -',
+  },
+  {
+    content: ' 如果',
+  },
+  {
+    content: '使用',
+  },
+  {
+    content: '新',
+  },
+  {
+    content: '鲜',
+  },
+  {
+    content: '螺',
+  },
+  {
+    content: '蛳',
+  },
+  {
+    content: '，',
+  },
+  {
+    content: '先',
+  },
+  {
+    content: '将',
+  },
+  {
+    content: '其',
+  },
+  {
+    content: '浸',
+  },
+  {
+    content: '泡',
+  },
+  {
+    content: '在',
+  },
+  {
+    content: '清',
+  },
+  {
+    content: '水',
+  },
+  {
+    content: '中',
+  },
+  {
+    content: '，',
+  },
+  {
+    content: '加入',
+  },
+  {
+    content: '少',
+  },
+  {
+    content: '许',
+  },
+  {
+    content: '盐',
+  },
+  {
+    content: '，',
+  },
+  {
+    content: '静',
+  },
+  {
+    content: '置',
+  },
+  {
+    content: '2',
+  },
+  {
+    content: '小时',
+  },
+  {
+    content: '以',
+  },
+  {
+    content: '去',
+  },
+  {
+    content: '除',
+  },
+  {
+    content: '泥',
+  },
+  {
+    content: '沙',
+  },
+  {
+    content: '。\n',
+  },
+  {
+    content: '  ',
+  },
+  {
+    content: ' -',
+  },
+  {
+    content: ' 清',
+  },
+  {
+    content: '洗',
+  },
+  {
+    content: '干',
+  },
+  {
+    content: '净',
+  },
+  {
+    content: '后',
+  },
+  {
+    content: '，',
+  },
+  {
+    content: '放',
+  },
+  {
+    content: '入',
+  },
+  {
+    content: '锅',
+  },
+  {
+    content: '中',
+  },
+  {
+    content: '加',
+  },
+  {
+    content: '水',
+  },
+  {
+    content: '煮',
+  },
+  {
+    content: '沸',
+  },
+  {
+    content: '，',
+  },
+  {
+    content: '捞',
+  },
+  {
+    content: '出',
+  },
+  {
+    content: '备用',
+  },
+  {
+    content: '。\n\n',
+  },
+  {
+    content: '2',
+  },
+  {
+    content: '.',
+  },
+  {
+    content: ' **',
+  },
+  {
+    content: '煮',
+  },
+  {
+    content: '米',
+  },
+  {
+    content: '粉',
+  },
+  {
+    content: '**\n',
+  },
+  {
+    content: '  ',
+  },
+  {
+    content: ' -',
+  },
+  {
+    content: ' 将',
+  },
+  {
+    content: '螺',
+  },
+  {
+    content: '蛳',
+  },
+  {
+    content: '粉',
+  },
+  {
+    content: '放',
+  },
+  {
+    content: '入',
+  },
+  {
+    content: '沸',
+  },
+  {
+    content: '水',
+  },
+  {
+    content: '中',
+  },
+  {
+    content: '煮',
+  },
+  {
+    content: '3',
+  },
+  {
+    content: '-',
+  },
+  {
+    content: '5',
+  },
+  {
+    content: '分钟',
+  },
+  {
+    content: '，',
+  },
+  {
+    content: '直到',
+  },
+  {
+    content: '米',
+  },
+  {
+    content: '粉',
+  },
+  {
+    content: '变',
+  },
+  {
+    content: '软',
+  },
+  {
+    content: '。\n',
+  },
+  {
+    content: '  ',
+  },
+  {
+    content: ' -',
+  },
+  {
+    content: ' 煮',
+  },
+  {
+    content: '好',
+  },
+  {
+    content: '后',
+  },
+  {
+    content: '，用',
+  },
+  {
+    content: '冷',
+  },
+  {
+    content: '水',
+  },
+  {
+    content: '冲',
+  },
+  {
+    content: '洗',
+  },
+  {
+    content: '，',
+  },
+  {
+    content: '沥',
+  },
+  {
+    content: '干',
+  },
+  {
+    content: '水',
+  },
+  {
+    content: '分',
+  },
+  {
+    content: '，',
+  },
+  {
+    content: '放',
+  },
+  {
+    content: '入',
+  },
+  {
+    content: '碗',
+  },
+  {
+    content: '中',
+  },
+  {
+    content: '备用',
+  },
+  {
+    content: '。\n\n',
+  },
+  {
+    content: '3',
+  },
+  {
+    content: '.',
+  },
+  {
+    content: ' **',
+  },
+  {
+    content: '准备',
+  },
+  {
+    content: '配',
+  },
+  {
+    content: '料',
+  },
+  {
+    content: '**\n',
+  },
+  {
+    content: '  ',
+  },
+  {
+    content: ' -',
+  },
+  {
+    content: ' 将',
+  },
+  {
+    content: '酸',
+  },
+  {
+    content: '笋',
+  },
+  {
+    content: '切',
+  },
+  {
+    content: '成',
+  },
+  {
+    content: '小',
+  },
+  {
+    content: '段',
+  },
+  {
+    content: '，',
+  },
+  {
+    content: '香',
+  },
+  {
+    content: '菜',
+  },
+  {
+    content: '和',
+  },
+  {
+    content: '青',
+  },
+  {
+    content: '蒜',
+  },
+  {
+    content: '切',
+  },
+  {
+    content: '碎',
+  },
+  {
+    content: '，',
+  },
+  {
+    content: '花',
+  },
+  {
+    content: '生',
+  },
+  {
+    content: '稍',
+  },
+  {
+    content: '微',
+  },
+  {
+    content: '捣',
+  },
+  {
+    content: '碎',
+  },
+  {
+    content: '备用',
+  },
+  {
+    content: '。\n\n',
+  },
+  {
+    content: '4',
+  },
+  {
+    content: '.',
+  },
+  {
+    content: ' **',
+  },
+  {
+    content: '调',
+  },
+  {
+    content: '制',
+  },
+  {
+    content: '汤',
+  },
+  {
+    content: '底',
+  },
+  {
+    content: '**\n',
+  },
+  {
+    content: '  ',
+  },
+  {
+    content: ' -',
+  },
+  {
+    content: ' 在',
+  },
+  {
+    content: '锅',
+  },
+  {
+    content: '中',
+  },
+  {
+    content: '加入',
+  },
+  {
+    content: '适',
+  },
+  {
+    content: '量',
+  },
+  {
+    content: '的',
+  },
+  {
+    content: '水',
+  },
+  {
+    content: '，',
+  },
+  {
+    content: '放',
+  },
+  {
+    content: '入',
+  },
+  {
+    content: '煮',
+  },
+  {
+    content: '好的',
+  },
+  {
+    content: '螺',
+  },
+  {
+    content: '蛳',
+  },
+  {
+    content: '，',
+  },
+  {
+    content: '加入',
+  },
+  {
+    content: '盐',
+  },
+  {
+    content: '、',
+  },
+  {
+    content: '鸡',
+  },
+  {
+    content: '精',
+  },
+  {
+    content: '和',
+  },
+  {
+    content: '醋',
+  },
+  {
+    content: '调',
+  },
+  {
+    content: '味',
+  },
+  {
+    content: '。\n',
+  },
+  {
+    content: '  ',
+  },
+  {
+    content: ' -',
+  },
+  {
+    content: ' 根据',
+  },
+  {
+    content: '个人',
+  },
+  {
+    content: '口',
+  },
+  {
+    content: '味',
+  },
+  {
+    content: '，可以',
+  },
+  {
+    content: '加入',
+  },
+  {
+    content: '高',
+  },
+  {
+    content: '汤',
+  },
+  {
+    content: '以',
+  },
+  {
+    content: '增加',
+  },
+  {
+    content: '鲜',
+  },
+  {
+    content: '味',
+  },
+  {
+    content: '。\n\n',
+  },
+  {
+    content: '5',
+  },
+  {
+    content: '.',
+  },
+  {
+    content: ' **',
+  },
+  {
+    content: '组',
+  },
+  {
+    content: '装',
+  },
+  {
+    content: '螺',
+  },
+  {
+    content: '蛳',
+  },
+  {
+    content: '粉',
+  },
+  {
+    content: '**\n',
+  },
+  {
+    content: '  ',
+  },
+  {
+    content: ' -',
+  },
+  {
+    content: ' 在',
+  },
+  {
+    content: '盛',
+  },
+  {
+    content: '有',
+  },
+  {
+    content: '米',
+  },
+  {
+    content: '粉',
+  },
+  {
+    content: '的',
+  },
+  {
+    content: '碗',
+  },
+  {
+    content: '中',
+  },
+  {
+    content: '，',
+  },
+  {
+    content: '加入',
+  },
+  {
+    content: '适',
+  },
+  {
+    content: '量',
+  },
+  {
+    content: '的',
+  },
+  {
+    content: '酸',
+  },
+  {
+    content: '笋',
+  },
+  {
+    content: '、',
+  },
+  {
+    content: '螺',
+  },
+  {
+    content: '蛳',
+  },
+  {
+    content: '、',
+  },
+  {
+    content: '花',
+  },
+  {
+    content: '生',
+  },
+  {
+    content: '、',
+  },
+  {
+    content: '香',
+  },
+  {
+    content: '菜',
+  },
+  {
+    content: '和',
+  },
+  {
+    content: '青',
+  },
+  {
+    content: '蒜',
+  },
+  {
+    content: '。\n',
+  },
+  {
+    content: '  ',
+  },
+  {
+    content: ' -',
+  },
+  {
+    content: ' 倒',
+  },
+  {
+    content: '入',
+  },
+  {
+    content: '调',
+  },
+  {
+    content: '好的',
+  },
+  {
+    content: '汤',
+  },
+  {
+    content: '底',
+  },
+  {
+    content: '，',
+  },
+  {
+    content: '最后',
+  },
+  {
+    content: '根据',
+  },
+  {
+    content: '个人',
+  },
+  {
+    content: '口',
+  },
+  {
+    content: '味',
+  },
+  {
+    content: '淋',
+  },
+  {
+    content: '上',
+  },
+  {
+    content: '辣',
+  },
+  {
+    content: '椒',
+  },
+  {
+    content: '油',
+  },
+  {
+    content: '。\n\n',
+  },
+  {
+    content: '6',
+  },
+  {
+    content: '.',
+  },
+  {
+    content: ' **',
+  },
+  {
+    content: '享',
+  },
+  {
+    content: '用',
+  },
+  {
+    content: '**\n',
+  },
+  {
+    content: '  ',
+  },
+  {
+    content: ' -',
+  },
+  {
+    content: ' 搅',
+  },
+  {
+    content: '拌',
+  },
+  {
+    content: '均',
+  },
+  {
+    content: '匀',
+  },
+  {
+    content: '后',
+  },
+  {
+    content: '，',
+  },
+  {
+    content: '螺',
+  },
+  {
+    content: '蛳',
+  },
+  {
+    content: '粉',
+  },
+  {
+    content: '就',
+  },
+  {
+    content: '可以',
+  },
+  {
+    content: '享',
+  },
+  {
+    content: '用了',
+  },
+  {
+    content: '。',
+  },
+  {
+    content: '可以',
+  },
+  {
+    content: '根据',
+  },
+  {
+    content: '个人',
+  },
+  {
+    content: '喜',
+  },
+  {
+    content: '好',
+  },
+  {
+    content: '添加',
+  },
+  {
+    content: '更多',
+  },
+  {
+    content: '的',
+  },
+  {
+    content: '调',
+  },
+  {
+    content: '料',
+  },
+  {
+    content: '。\n\n',
+  },
+  {
+    content: '##',
+  },
+  {
+    content: ' 注意',
+  },
+  {
+    content: '事项',
+  },
+  {
+    content: '\n',
+  },
+  {
+    content: '-',
+  },
+  {
+    content: ' 螺',
+  },
+  {
+    content: '蛳',
+  },
+  {
+    content: '粉',
+  },
+  {
+    content: '的',
+  },
+  {
+    content: '汤',
+  },
+  {
+    content: '底',
+  },
+  {
+    content: '可以',
+  },
+  {
+    content: '根据',
+  },
+  {
+    content: '个人',
+  },
+  {
+    content: '口',
+  },
+  {
+    content: '味',
+  },
+  {
+    content: '进行',
+  },
+  {
+    content: '调整',
+  },
+  {
+    content: '，',
+  },
+  {
+    content: '喜欢',
+  },
+  {
+    content: '酸',
+  },
+  {
+    content: '辣',
+  },
+  {
+    content: '的',
+  },
+  {
+    content: '可以',
+  },
+  {
+    content: '多',
+  },
+  {
+    content: '加',
+  },
+  {
+    content: '醋',
+  },
+  {
+    content: '和',
+  },
+  {
+    content: '辣',
+  },
+  {
+    content: '椒',
+  },
+  {
+    content: '油',
+  },
+  {
+    content: '。\n',
+  },
+  {
+    content: '-',
+  },
+  {
+    content: ' 确',
+  },
+  {
+    content: '保',
+  },
+  {
+    content: '螺',
+  },
+  {
+    content: '蛳',
+  },
+  {
+    content: '清',
+  },
+  {
+    content: '洗',
+  },
+  {
+    content: '干',
+  },
+  {
+    content: '净',
+  },
+  {
+    content: '，以',
+  },
+  {
+    content: '免',
+  },
+  {
+    content: '影响',
+  },
+  {
+    content: '口',
+  },
+  {
+    content: '感',
+  },
+  {
+    content: '。\n\n',
+  },
+  {
+    content: '通过',
+  },
+  {
+    content: '以上',
+  },
+  {
+    content: '步骤',
+  },
+  {
+    content: '，',
+  },
+  {
+    content: '您',
+  },
+  {
+    content: '就',
+  },
+  {
+    content: '可以',
+  },
+  {
+    content: '在',
+  },
+  {
+    content: '家',
+  },
+  {
+    content: '制作',
+  },
+  {
+    content: '出',
+  },
+  {
+    content: '美',
+  },
+  {
+    content: '味',
+  },
+  {
+    content: '的',
+  },
+  {
+    content: '螺',
+  },
+  {
+    content: '蛳',
+  },
+  {
+    content: '粉',
+  },
+  {
+    content: '，',
+  },
+  {
+    content: '享',
+  },
+  {
+    content: '受',
+  },
+  {
+    content: '这',
+  },
+  {
+    content: '道',
+  },
+  {
+    content: '独',
+  },
+  {
+    content: '特',
+  },
+  {
+    content: '的',
+  },
+  {
+    content: '美',
+  },
+  {
+    content: '食',
+  },
+  {
+    content: '。\n',
+  },
+  {
+    content: '</',
+  },
+  {
+    content: 'ref',
+  },
+  {
+    content: 'ly',
+  },
+  {
+    content: 'Canvas',
+  },
+];
