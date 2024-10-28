c1a709743b84d04ab8dac36c4decf159b339118a
pftom <1043269994@qq.com>
2024-10-28 10:57:59 +0800

    feat(skill-template): add start event

--- packages/skill-template/src/scheduler/index.ts
+++ packages/skill-template/src/scheduler/index.ts
@@ -336,6 +336,7 @@ Please generate the summary based on these requirements and offer suggestions fo
     const { chatHistory = [], currentSkill, spanId } = config.configurable;
     const { user } = config;
 
+    this.emitEvent({ event: 'start' }, this.configSnapshot);
     this.emitEvent({ event: 'log', content: `Start to generate canvas...` }, config);
 
     const { convId, projectId = '' } = this.configSnapshot.configurable;
