744f10b42a101042e5dd217fbc75849ad4bc4ea6
mrcfps <mrc@powerformer.com>
2024-11-14 10:19:02 +0800

    fix(api): adjust prisma log

--- apps/api/src/common/prisma.service.ts
+++ apps/api/src/common/prisma.service.ts
@@ -14,14 +14,14 @@ export class PrismaService extends PrismaClient implements OnModuleInit {
         },
       ],
     });
-
-    this.$on('query' as never, (e: any) => {
-      this.logger.log(`query: ${e.query}, param: ${e.params}, duration: ${e.duration}ms`);
-    });
   }
 
   async onModuleInit() {
     await this.$connect();
     this.logger.log('Connected to database');
+
+    this.$on('query' as never, (e: any) => {
+      this.logger.log(`query: ${e.query}, param: ${e.params}, duration: ${e.duration}ms`);
+    });
   }
 }
