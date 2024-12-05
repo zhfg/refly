626512c10560624a6db44225271f8803cfea886c
pftom <1043269994@qq.com>
2024-12-05 20:25:17 +0800

    feat(web): add logout for request-access

---
+++ packages/ai-workspace-common/src/hooks/useLogout.ts
@@ -0,0 +1,47 @@
+import { useTranslation } from 'react-i18next';
+import { Modal } from '@arco-design/web-react';
+import { useCookie } from 'react-use';
+import Cookies from 'js-cookie';
+import { getCookieOrigin, getExtensionId } from '@refly/utils/url';
+import { useUserStore } from '@refly-packages/ai-workspace-common/stores/user';
+
+export const useLogout = () => {
+  const { t } = useTranslation();
+  const userStore = useUserStore();
+  const [modal, contextHolder] = Modal.useModal();
+  const [_, __, deleteCookie] = useCookie('_refly_ai_sid');
+
+  const handleLogout = () => {
+    modal.confirm?.({
+      okText: t('common.confirm'),
+      cancelText: t('common.cancel'),
+      title: t('settings.account.logoutConfirmation.title'),
+      content: t('settings.account.logoutConfirmation.message'),
+      onOk() {
+        // Clear local storage
+        localStorage.removeItem('refly-user-profile');
+        localStorage.removeItem('refly-local-settings');
+
+        // Notify extension
+        chrome.runtime?.sendMessage(getExtensionId(), {
+          name: 'external-refly-logout-notify',
+        });
+
+        // Clear cookies
+        deleteCookie();
+        Cookies.remove('_refly_ai_sid', { domain: getCookieOrigin() });
+
+        // Reset user store
+        userStore.resetState();
+
+        // Reload page
+        window.location.reload();
+      },
+    });
+  };
+
+  return {
+    handleLogout,
+    contextHolder,
+  };
+};
