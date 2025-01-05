274bc07127bc52df9173300de8d0447c2df00f48
mrcfps <mrc@powerformer.com>
2025-01-05 16:40:51 +0800

    fix(document): further cleanup image upload

--- packages/ai-workspace-common/src/components/editor/components/image-upload.ts
+++ packages/ai-workspace-common/src/components/editor/components/image-upload.ts
@@ -6,29 +6,11 @@ import { createImageUpload } from '@refly-packages/ai-workspace-common/component
 export const createUploadFn = (param: { entityId: string; entityType: string }) => {
   return createImageUpload({
     onUpload: async (file: File) => {
-      const formData = new FormData();
-      formData.append('file', file);
-      formData.append('entityId', param.entityId);
-      formData.append('entityType', param.entityType);
-
-      const token = document.cookie
-        .split('; ')
-        .find((row) => row.startsWith('_refly_ai_sid='))
-        ?.split('=')[1];
-
-      if (!token) {
-        message.error('No token found');
-        return false;
-      }
-
       const { data, error } = await getClient().upload({
         body: {
           file,
           entityId: param.entityId,
           entityType: param.entityType as EntityType,
-        },
-        headers: {
-          Authorization: `Bearer ${token}`,
         },
       });
 
