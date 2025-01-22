0b7255d429a13347b59d00fd0eb5526b7919a844
mrcfps <mrc@powerformer.com>
2025-01-22 17:31:25 +0800

    fix(web): clone and cache request beforehand to properly retry after token refresh

--- packages/ai-workspace-common/src/utils/request.ts
+++ packages/ai-workspace-common/src/utils/request.ts
@@ -1,6 +1,5 @@
 import { QueryClient } from '@tanstack/react-query';
 import { getServerOrigin } from '@refly/utils/url';
-import { safeParseJSON } from '@refly/utils/parse';
 import { client } from '@refly-packages/ai-workspace-common/requests';
 import { responseInterceptorWithTokenRefresh } from '@refly-packages/ai-workspace-common/utils/auth';
 
@@ -9,11 +8,6 @@ client.setConfig({
   credentials: 'include',
   throwOnError: false, // If you want to handle errors on `onError` callback of `useQuery` and `useMutation`, set this to `true`
 });
-
-const getLocale = () => {
-  const settings = safeParseJSON(localStorage.getItem('refly-local-settings'));
-  return settings?.uiLocale || 'en';
-};
 
 client.interceptors.response.use(async (response, request) => {
   return responseInterceptorWithTokenRefresh(response, request);
