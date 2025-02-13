dd5c6599840b6657d7c91c984684e80bcbd252cb
CH <ch@refly.ai>
2025-02-13 21:36:16 +0800

    feat(resource): Add custom file icon component and improve resource icon handling
    - Implement new FileIcon component with customizable styles
    - Add default styles for various file extensions
    - Update ResourceIcon component to use new FileIcon for file resources
    - Enhance resource list and resource view with improved icon rendering
    - Add colord and prop-types as dependencies for icon styling

--- packages/ai-workspace-common/src/components/common/resourceIcon.tsx
+++ packages/ai-workspace-common/src/components/common/resourceIcon.tsx
@@ -1,15 +1,41 @@
+import { useState } from 'react';
 import { ResourceType } from '@refly/openapi-schema';
 import { IconText } from '@refly-packages/ai-workspace-common/components/common/icon';
 import { BsFileRichtext } from 'react-icons/bs';
+import {
+  FileIcon,
+  defaultStyles,
+} from '@refly-packages/ai-workspace-common/components/common/resource-icon';
+import { IconResourceFilled } from '@refly-packages/ai-workspace-common/components/common/icon';
+import { NODE_COLORS } from '@refly-packages/ai-workspace-common/components/canvas/nodes/shared/colors';
 
-export const ResourceIcon = (props: { url: string; resourceType: ResourceType; size?: number }) => {
-  const { url, resourceType, size = 18 } = props;
+const FileExtensionIcon = Object.keys(defaultStyles);
+const getExtensionIcon = (extension: string, size: number) => {
+  const fileExtension = FileExtensionIcon.includes(extension) ? extension : 'doc';
+  return <FileIcon width={size} extension={fileExtension} {...defaultStyles[fileExtension]} />;
+};
+
+export const ResourceIcon = (props: {
+  url: string;
+  resourceType: ResourceType;
+  extension?: string;
+  size?: number;
+}) => {
+  const { url, resourceType, extension, size = 18 } = props;
+  const [showFallbackIcon, setShowFallbackIcon] = useState(false);
+
+  if (resourceType === 'file') {
+    return getExtensionIcon(extension, size);
+  }
   if (url) {
-    return (
+    return showFallbackIcon ? (
+      <IconResourceFilled color={NODE_COLORS.resource} size={size} />
+    ) : (
       <img
         style={{ width: size, height: size }}
         src={`https://www.google.com/s2/favicons?domain=${url}&sz=${size}`}
         alt={url}
+        onError={() => setShowFallbackIcon(true)}
       />
     );
   }
