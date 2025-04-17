d2048f6dbd4bb1de0339687b1a7844df5eb8b3fb
Siri-Ray <109605599+Siri-Ray@users.noreply.github.com>
2025-04-17 16:53:31 +0800

    fix(web): remove auto redirect in home page (#756)
    * fix/remove-auto-redirect
    * fix/remove-auto-redirect-2
    
    ---------
    
    Co-authored-by: xi rui <xirui0328@gmain.com>

--- apps/web/src/pages/canvas/index.tsx
+++ apps/web/src/pages/canvas/index.tsx
@@ -1,4 +1,4 @@
-import { useNavigate, useParams } from 'react-router-dom';
+import { useParams } from 'react-router-dom';
 import { Canvas } from '@refly-packages/ai-workspace-common/components/canvas';
 import { Button, Empty } from 'antd';
 import { useTranslation } from 'react-i18next';
@@ -7,24 +7,14 @@ import { useCreateCanvas } from '@refly-packages/ai-workspace-common/hooks/canva
 import { useSiderStoreShallow } from '@refly-packages/ai-workspace-common/stores/sider';
 import { SiderPopover } from '@refly-packages/ai-workspace-common/components/sider/popover';
 import { AiOutlineMenuUnfold } from 'react-icons/ai';
-import { useEffect } from 'react';
 const CanvasPage = () => {
   const { t } = useTranslation();
-  const navigate = useNavigate();
-
   const { canvasId = '' } = useParams();
   const { debouncedCreateCanvas, isCreating } = useCreateCanvas();
-  const { collapse, setCollapse, canvasList } = useSiderStoreShallow((state) => ({
-    canvasList: state.canvasList ?? [],
+  const { collapse, setCollapse } = useSiderStoreShallow((state) => ({
     collapse: state.collapse,
     setCollapse: state.setCollapse,
   }));
-
-  useEffect(() => {
-    if (canvasId === 'empty' && canvasList.length > 0) {
-      navigate(`/canvas/${canvasList[0].id}`, { replace: true });
-    }
-  }, [canvasId, canvasList, navigate]);
 
   return canvasId && canvasId !== 'empty' ? (
     <Canvas canvasId={canvasId} />
--- packages/ai-workspace-common/src/components/home-redirect/index.tsx
+++ packages/ai-workspace-common/src/components/home-redirect/index.tsx
@@ -4,7 +4,6 @@ import { SuspenseLoading } from '@refly-packages/ai-workspace-common/components/
 import { ReactNode } from 'react';
 import { Navigate } from 'react-router-dom';
 import { useUserStoreShallow } from '@refly-packages/ai-workspace-common/stores/user';
-import { useCanvasStore } from '@refly-packages/ai-workspace-common/stores/canvas';
 
 export const HomeRedirect = ({ defaultNode }: { defaultNode: ReactNode }) => {
   const [element, setElement] = useState<ReactNode | null>(null);
@@ -14,12 +13,6 @@ export const HomeRedirect = ({ defaultNode }: { defaultNode: ReactNode }) => {
 
   const handleHomeRedirect = async () => {
     if (isLogin) {
-      const { currentCanvasId } = useCanvasStore.getState();
-
-      if (currentCanvasId) {
-        return <Navigate to={`/canvas/${currentCanvasId}`} replace />;
-      }
-
       return <Navigate to={'/canvas/empty'} replace />;
     }
     return defaultNode;
