0859a8c971abb30ea2eaa8eb103f3a823d653fcd
Siri-Ray <109605599+Siri-Ray@users.noreply.github.com>
2025-04-18 15:30:51 +0800

    fix(api): db url parsing issues for sync-db-schema script (#773)
    Co-authored-by: xi rui <xirui0328@gmain.com>

--- apps/api/src/scripts/sync-db-schema.ts
+++ apps/api/src/scripts/sync-db-schema.ts
@@ -1,6 +1,6 @@
 import { execSync } from 'node:child_process';
 
 execSync(
-  `npx prisma migrate diff --from-url '${process.env.DATABASE_URL}' --to-schema-datamodel prisma/schema.prisma --script | npx prisma db execute --stdin`,
+  `npx prisma migrate diff --from-url ${process.env.DATABASE_URL} --to-schema-datamodel prisma/schema.prisma --script | npx prisma db execute --stdin`,
   { stdio: 'inherit' },
 );
